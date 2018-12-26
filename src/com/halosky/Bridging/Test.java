aa0e373d8a0cd02d77ae80e24ff093c221dac0d0
halosky-Wendy <626090763@qq.com>
2018-12-26 19:18:17 +0800

    桥接者模式

--- src/com/halosky/Bridging/Test.java
+++ src/com/halosky/Bridging/Test.java
@@ -1,7 +1,10 @@
 package com.halosky.Bridging;
 
 import com.halosky.Bridging.abstracts.AreaA;
+import com.halosky.Bridging.core.AreaA1;
 import com.halosky.Bridging.core.AreaA2;
+import com.halosky.Bridging.interfaceImpl.AreaB1;
+import com.halosky.Bridging.interfaces.Qiao;
 
 /**
  * @Author YangHuan
@@ -11,6 +14,10 @@ public class Test {
 
     public static void main(String[] args){
         AreaA a = new AreaA2();
+        AreaA a1 = new AreaA1();
+
+        Qiao qiao = new AreaB1();
+
     }
 
 }
